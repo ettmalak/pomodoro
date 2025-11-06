be5eb7af376940161594518aa717664e5205b80c
Malak Ettabia <malakettabia00@gmail.com>
2025-11-06 15:57:18 +0100

    add buttons

--- main.py
+++ main.py
@@ -20,17 +20,26 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+label = Label(text="Timer", font=(FONT_NAME, 45, "bold"), bg=YELLOW, fg=GREEN)
+label.config(pady=10)
+label.pack()
+
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112,  image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
+check_Mark = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
+check_Mark.place(x=85, y=320)
+
+start_btn = Button(text="Start", bg="white")
+start_btn.place(x=0, y=320)
 
 
-
-
-
+start_btn = Button(text="Reset", bg="white")
+start_btn.place(x=190, y=320)
 
 
 window.mainloop()
