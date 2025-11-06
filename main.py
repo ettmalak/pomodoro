c794cb7e5e4a0d355a0da17049dd8bf2bebb9e29
Malak Ettabia <malakettabia00@gmail.com>
2025-11-06 19:24:44 +0100

    add count down mechanism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,34 +13,45 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5*60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+
+    count_min = math.floor(count/60)
+    count_sec = count%60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-label = Label(text="Timer", font=(FONT_NAME, 45, "bold"), bg=YELLOW, fg=GREEN)
-label.config(pady=10)
-label.pack()
+
+label = Label(text="Timer", font=(FONT_NAME, 50, "bold"), bg=YELLOW, fg=GREEN)
+label.grid(column=1, row=0)
 
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112,  image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+timer_text=canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_btn = Button(text="Start", bg="white", highlightthickness=0, command=start_timer)
+start_btn.grid(column=0, row=2)
+
+
+reset_btn = Button(text="Reset", bg="white", highlightthickness=0)
+reset_btn.grid(column=2, row=2)
+
 
 check_Mark = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
-check_Mark.place(x=85, y=320)
-
-start_btn = Button(text="Start", bg="white")
-start_btn.place(x=0, y=320)
-
-
-start_btn = Button(text="Reset", bg="white")
-start_btn.place(x=190, y=320)
+check_Mark.grid(column=1, row=3)
 
 
 window.mainloop()
